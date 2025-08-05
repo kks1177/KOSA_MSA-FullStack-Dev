2ebb6a2aa94b1acf97f0b1be1946988c1a93f0ae
kks1177 <kks1177.dev@gmail.com>
2025-08-05 09:38:42 +0900

    250805_SpringFramework class

---
+++ 05_SpringFramework/250804_Spring/src/main/java/org/zerock/sample/SampleHotel2.java
@@ -0,0 +1,22 @@
+// SampleHotel2.java
+
+package org.zerock.sample;
+
+import org.springframework.stereotype.Component;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Component
+@ToString
+@Getter
+@AllArgsConstructor
+public class SampleHotel2 {
+    private Chef chef;  
+}//end class
+
+
+
+
+
+
